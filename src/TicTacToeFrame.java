2c3f21acb8a789b7ba9165e2764a51664e8a84a8
aidenward6 <aidenjward921@gmail.com>
2024-02-26 19:30:22 -0500

    Assignment

---
+++ src/TicTacToeFrame.java
@@ -0,0 +1,125 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToeFrame extends JFrame {
+
+    private JButton[][] buttons;
+    private char currentPlayer;
+    private int moves;
+
+    public TicTacToeFrame() {
+        super("Tic-Tac-Toe");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLayout(new GridLayout(4, 3));
+
+        buttons = new JButton[3][3];
+        currentPlayer = 'X';
+        moves = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j] = new JButton("");
+                buttons[i][j].setFont(new Font("Serif", Font.BOLD, 100));
+                buttons[i][j].addActionListener(new ButtonClickListener(i, j));
+                add(buttons[i][j]);
+            }
+        }
+
+        JButton quitButton = new JButton("Quit");
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        add(quitButton);
+        setVisible(true );
+    }
+
+    private class ButtonClickListener implements ActionListener {
+        private int row, col;
+
+        public ButtonClickListener(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton button = (JButton) e.getSource();
+
+            if (button.getText().equals("")) {
+                button.setText(String.valueOf(currentPlayer));
+                moves++;
+
+                if (checkWin()) {
+                    JOptionPane.showMessageDialog(null, "Player " + currentPlayer + " wins!");
+                    resetGame();
+                } else if (checkDraw()) {
+                    JOptionPane.showMessageDialog(null, "It's a draw!");
+                    resetGame();
+                } else {
+                    switchPlayer();
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid move! Try again.");
+            }
+        }
+    }
+
+    private boolean checkWin() {
+        return checkRows() || checkColumns() || checkDiagonals();
+    }
+
+    private boolean checkRows() {
+        for (int i = 0; i < 3; i++) {
+            if (buttons[0][i].getText().equals(String.valueOf(currentPlayer))
+                    && buttons[1][i].getText().equals(String.valueOf(currentPlayer))
+                    && buttons[2][i].getText().equals(String.valueOf(currentPlayer))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkColumns() {
+        for (int i = 0; i < 3; i++) {
+            if (buttons[i][0].getText().equals(String.valueOf(currentPlayer))
+                    && buttons[i][1].getText().equals(String.valueOf(currentPlayer))
+                    && buttons[i][2].getText().equals(String.valueOf(currentPlayer))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDiagonals() {
+        return (buttons[0][0].getText().equals(String.valueOf(currentPlayer))
+                && buttons[1][1].getText().equals(String.valueOf(currentPlayer))
+                && buttons[2][2].getText().equals(String.valueOf(currentPlayer)))
+                || (buttons[0][2].getText().equals(String.valueOf(currentPlayer))
+                && buttons[1][1].getText().equals(String.valueOf(currentPlayer))
+                && buttons[2][0].getText().equals(String.valueOf(currentPlayer)));
+    }
+
+    private boolean checkDraw() {
+        return moves >= 9;
+    }
+
+    private void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    private void resetGame() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+        currentPlayer = 'X';
+        moves = 0;
+    }
+}
